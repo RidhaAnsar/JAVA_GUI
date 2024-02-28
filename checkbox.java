02e028f6f223471754b6c05ea027820fb765575a
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-02-28 20:28:24 +0530

    Add files via upload

---
+++ checkbox.java
@@ -0,0 +1,62 @@
+//checkbox
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+class CCB extends Frame implements ActionListener {
+	Button bsel, b3;
+	TextField t1;
+	Checkbox c1,c2,c3;
+	public CCB() {
+		super("CHECKBOX DEMO");
+		t1=new TextField(10);
+		bsel=new Button("SELECTED");
+		b2=new Button("Exit");
+		c1=new Checkbox("UG");
+		c1=new Checkbox("PG");
+		c1=new Checkbox("PHD");
+		setLayout(new FlowLayout());
+		add(c1);
+		add(c2);
+		add(c3);
+		add(bsel);
+		add(b3);
+		bsel.addActionListener(this);
+		b3.addActionListener(this);
+		
+		
+		setSize(400,400);
+		setVisible(true);
+	}
+	public void actionPerformed(ActionEvent ae)
+	{
+		if(ae.getSource() == b3)
+		{
+			System.exit(0);
+		}
+		else if(ae.getSource() == bsel)
+		{
+			String str = "";
+			if(c1.getState() == true)
+			{
+				str = c1.getLabel();
+			}
+			if(c2.getState() == true)
+			{
+			str = str + " " + c2.getLabel();
+			}
+			if(c3.getState() == true)
+			{
+			str = str + " " + c3.getLabel();
+			}
+			t1.setText(str);
+		}
+	}
+}
+class CheckBoxDemo
+{
+	public static void main(String args[])
+	{
+		CBD obj = new CBD();
+	}
+}
---
+++ listboxdemo.java
@@ -0,0 +1,92 @@
+//listboxdemo
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class CLB extends Frame implements ActionListener, ItemListener  {
+	Button badd, bsel, bdel, b3;
+	TextField t1;
+	List l;
+
+	public CLB() {
+		super("Listbox demo");
+		t1=new TextField(10);
+		l=new List(10);
+		badd=new Button ("ADD");
+		bsel=new Button("SELECTED");
+		bdel=new Button("DELETED");
+		b3=new Button ("EXIT");
+		setLayout(new FlowLayout());
+		add(l);
+		add(t1);
+		add(badd);
+		add(bsel);
+		add(b3);
+	badd.addActionListener(this);
+	bsel.addActionListener(this);
+	bdel.addActionListener(this);
+	b3.addActionListener(this);
+	l.addActionListener(this);
+	this.addWindowListener(new WindowAdapter() {
+        		public void windowClosing(WindowEvent windowEvent){
+            			setVisible(false);
+				System.exit(0);
+         		}
+		});
+
+	setSize(200,200);
+	setVisible(true);
+	}
+public void actionPerformed(ActionEvent ae)
+ {
+ 	if(ae.getSource()==b3)  {
+		System.exit(0);
+	}
+else if(ae.getSource() == badd)
+		{
+			String str = t1.getText();
+			l.add(str);
+			t1.setText("");
+		}
+		else if(ae.getSource() == bsel)
+		{
+			int pos;
+			pos = l.getSelectedIndex();
+			if(pos != -1)
+			{
+				String str = (String)l.getSelectedItem();
+				t1.setText(str);
+			}
+		}
+		else if(ae.getSource() == bdel)
+		{
+			int pos;
+			pos = l.getSelectedIndex();
+			if(pos != -1)
+			{
+				l.remove(pos);
+			}
+		}
+	}	
+	public void itemStateChanged(ItemEvent ie)
+	{
+		if(ie.getSource() == l)
+		{
+			String str = (String)l.getSelectedItem();
+			t1.setText(str);
+		}
+	}
+}
+class ListBoxDemo
+{
+	public static void main(String args[])
+	{
+		CLB obj = new CLB();
+	}
+}
+
+
+
+
+
